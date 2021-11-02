f9e815928945a5ed3df789848ecf16a9d3ebb9a8
De Mot Daan <daan.demot44@gmaim.com>
2021-11-02 16:44:07 +0100

    Afwerken footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,16 +2,18 @@ import * as React from 'react'
 import { Link } from 'gatsby'
 import { useStaticQuery, graphql } from 'gatsby'
 import { container } from './layout.module.css'
+import Footer from './footer'
 import {
   nav,
   heading,
   navLinks,
   navLinkItem,
-  navLinkText
+  navLinkText,
+  siteTitle
 } from './layout.module.css'
 
 
-const Layout = ({ pageTitle, children }) => {
+const Layout = ({ children }) => {
 
 
   const data = useStaticQuery(graphql`
@@ -21,37 +23,55 @@ const Layout = ({ pageTitle, children }) => {
         title
       }
     }
+    wpPage(slug: { eq: "contact-us" }) {
+      contactPage {
+        companyInformation {
+          address
+          city
+          postcode
+          socials {
+            facebook
+            instagram
+          }
+        }
+      }
+    }
+
   }
   `)
 
   return (
-   <div className={container}>
-      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-      <header>{data.site.siteMetadata.title}</header>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}>
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
-              About
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/artists">
-              Artists
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <h1 className={heading}>{pageTitle}</h1>
-        {children}
-      </main>
-    </div>
+    <>
+      <div className={container}>
+        <title>{data.site.siteMetadata.title}</title>
+        <nav className={nav}>
+          <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+          <ul className={navLinks}>
+            <li></li>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/">
+                Home
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/about">
+                About
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/artists">
+                Artists
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <main>{children}</main>
+      </div>
+      <Footer
+        siteTitle={data.site.siteMetadata.title}
+        companyInfo={data.wpPage.contactPage.companyInformation}
+      />
+    </>
   )
 }
 export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,120 @@
 import * as React from 'react'
 
 import Layout from '../components/layout'
+import Artist from '../components/artist'
 import { StaticImage } from 'gatsby-plugin-image'
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { graphql } from 'gatsby'
+import {
+  header,
+  headerInfo,
+  headerPicture,
+  headerTitle,
+  CTA,
+  section,
+  subtitle,
+  artists,
+} from '../components/page.module.css'
 
-const IndexPage = () => {
+const IndexPage = ({
+  data: {
+    wpPage: { homePage },
+  },
+}) => {
+  const image = getImage(homePage.headerHome.picture.localFile)
   return (
-    <Layout pageTitle="Home Page">
-      <h1>Welcome to De Mot Agency!</h1>
-      <p>Lorem ipsum</p>
-      <StaticImage
-        alt="randomized unsplash image!"
-        src="../images/martha.jpg"
-      />
+    <Layout>
+      <div className={header}>
+        <div className={headerInfo}>
+          <h1 className={headerTitle}> {homePage.headerHome.title}</h1>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: homePage.headerHome.description,
+            }}
+          />
+          <a className={CTA} target="__blank" href={homePage.callToAction.link}>
+            {homePage.callToAction.description}
+          </a>
+        </div>
+
+        
+        <div>
+        <GatsbyImage
+            image={image}
+            className={headerPicture}
+            alt={homePage.headerHome.picture.altText}
+          />
+        </div>
+      </div>
+
+      <div className={section}>
+    <h2 className={subtitle}>{homePage.featuredArtists.title}</h2>
+    <p>{homePage.featuredArtists.description}</p>
+    <div className={artists}>
+    
+      {homePage.featuredArtists.artists.map(artist => (
+            
+             <Artist key={artist.id} artist={artist} />
+             // <Artist slug={`artists/${slug}`} key={artist.id} artist={artist} /> slug wordt niet gevonden ookal steekt die wel id query, maar oke zonder gaat ook.
+          ))}
+        
+    </div>
+  </div>
     </Layout>
   )
+
 }
+
+export const queryHomePage = graphql`
+query  {
+  wpPage(slug: {eq: "home"}) {
+    homePage {
+      headerHome {
+        description
+        title
+        picture {
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(
+                placeholder: BLURRED
+                width: 500
+                jpgOptions: {quality: 10}
+                height: 500
+              )
+            }
+          }
+        }
+      }
+      callToAction {
+        description
+        link
+      }
+      featuredArtists {
+        artists {
+          ... on WpArtist {
+            id
+            artistMeta {
+              artistName
+              firstName
+              lastName
+              profilePicture {
+                localFile {
+                  childImageSharp {
+                    gatsbyImageData(placeholder: BLURRED, transformOptions: {grayscale: false})
+                  }
+                }
+              }
+            }
+            slug
+          }
+        }
+        description
+        title
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
